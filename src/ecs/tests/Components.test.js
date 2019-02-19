6fa27d26a3cbb4ad401ca2c7dff429a249271d73
Eeshan Garg <jerryguitarist@gmail.com>
2019-02-19 13:35:41 -0330

    Components.js: Add static INDEX value.
    This static value will be used as an index into an array instance
    variable by the Entity class.

--- src/ecs/tests/Components.test.js
+++ src/ecs/tests/Components.test.js
@@ -12,18 +12,32 @@ const Vec = require('../Vec.js');
 
 test('Components', () => {
     var transform = new CTransform(new Vec(2.0, 3.0));
+    expect(CTransform.INDEX).toBe(0);
+
     var input = new CInput();
+    expect(CInput.INDEX).toBe(1);
 
     var box = new CBoundingBox(new Vec(40, 40), true, true);
     expect(box.halfSize.x).toBe(20);
     expect(box.halfSize.y).toBe(20);
+    expect(CBoundingBox.INDEX).toBe(2);
 
     var gravity = new CGravity(2.0);
+    expect(CGravity.INDEX).toBe(3);
+
     var health = new CHealth();
     expect(health.health).toBe(100);
+    expect(CHealth.INDEX).toBe(4);
 
     var state = new CState('stand');
+    expect(CState.INDEX).toBe(5);
+
     var draggable = new CDraggable();
+    expect(CDraggable.INDEX).toBe(6);
+
     var follow = new CFollow(new Vec(2.0, 3.0), 5.0);
+    expect(CFollow.INDEX).toBe(7);
+
     var patrol = new CPatrol([1.0, 2.0, 3.0, 4.0], 5.0);
+    expect(CPatrol.INDEX).toBe(8);
 });
