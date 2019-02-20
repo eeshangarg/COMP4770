ec663ccf1bfbb67e9deef29a451ef59e709422e9
Eeshan Garg <jerryguitarist@gmail.com>
2019-02-20 15:40:53 -0330

    ecs: Add Entity class.

---
+++ src/ecs/tests/Entity.test.js
@@ -0,0 +1,20 @@
+const Components = require('../Components.js');
+const CInput = Components.CInput;
+const Entity = require('../Entity.js');
+
+test('Entity', () => {
+    var entity = new Entity(0, 'tile');
+
+    expect(entity.hasComponent(CInput)).toBeFalsy();
+    entity.addComponent(new CInput());
+    expect(entity.hasComponent(CInput)).toBeTruthy();
+    const input = entity.getComponent(CInput);
+    expect(input.up).toBeFalsy();
+    entity.removeComponent(CInput);
+    expect(entity.hasComponent(CInput)).toBeFalsy();
+    expect(entity.getComponent(CInput)).toBe(null);
+
+    expect(entity.isActive()).toBeTruthy();
+    entity.destroy();
+    expect(entity.isActive()).toBeFalsy();
+});
