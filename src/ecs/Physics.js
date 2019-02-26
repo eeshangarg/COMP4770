ad04e946916228df10fc4200f718c0e36a940d17
Pranav <pranavp2810@Pranavs-MacBook-Pro.local>
2019-02-26 15:11:10 -0330

    Add Physics version 2

--- src/ecs/Physics.js
+++ src/ecs/Physics.js
@@ -2,94 +2,104 @@
 /* global module */
 /* global require */
 
-const Components = require('./Components.js');
-const CBoundingBox = Components.CBoundingBox;
-const CTransform = Components.CTransform;
+//const Components = require('./Components.js');
+//const CBoundingBox = Components.CBoundingBox;
+//const CTransform = Components.CTransform;
 const Vec = require('./Vec.js');
 const Entity = require('./Entity.js');
 
 
-function getOverlap(a: Array<Entity>, b: Array<Entity>): Vec {
-	var aBB: Vec = a.getComponent(CBoundingBox).halfSize;
-	var aPos: Vec = a.getComponent(CTransform).pos;
-	var bBB: Vec = b.getComponent(CBoundingBox).halfSize;
-	var bPos: Vec = b.getComponent(CTransform).pos;
-	return Vec((aBB.x + bBB.x) - Math.abs(bPos.x - aPos.x), (aBB.y + bBB.y) - Math.abs(bPos.y - aPos.y));
+function getOverlap(a: Entity, b: Entity): Vec {
 
+	var aPos: Vec = a.getComponent.CTransform().pos;
+    //var y1: Vec = a.getComponent.CTransform().pos.y;
+    var bPos: Vec = b.getComponent.CTransform().pos;
+   // var y2: Vec = b.getComponent.CTransform().pos.y;
+	//Vec delta(abs(x1-x2), abs(y1-y2));
+	var negDelta: Vec = aPos.subtract(bPos);
+	var delta: Vec = negDelta.abs();
+   // var w1: Vec = a.getComponent.CBoundingBox().halfSize.x;
+	//var h1: Vec = a.getComponent.CBoundingBox().halfSize.y;
+	var w1h1: Vec = a.getComponent.CBoundingBox().halfSize;
+    //var w2: Vec = b.getComponent.CBoundingBox().halfSize.x;
+	//var h2: Vec = b.getComponent.CBoundingBox().halfSize.y;
+	var w2h2: Vec = b.getComponent.CBoundingBox().halfSize;
+    //var ox: Vec = w1 + w2 - delta.x;
+	//var oy: Vec = h1 + h2 - delta.y;
+	var oxy: Vec = w1h1.add(w2h2).subtract(delta);
+	return oxy; //Vec(ox, oy);
 }
 
-function getPreviousOverlap(a: Array<Entity>, b: Array<Entity>): Vec {
-	var aBB: Vec = a.getComponent(CBoundingBox).halfSize;
-	var aPos: Vec = a.getComponent(CTransform).prevPos;
-	var bBB: Vec = b.getComponent(CBoundingBox).halfSize;
-	var bPos: Vec = b.getComponent(CTransform).pos;
-	return Vec((aBB.x + bBB.x) - Math.abs(bPos.x - aPos.x), (aBB.y + bBB.y) - Math.abs(bPos.y - aPos.y));
-
+function getPreviousOverlap(a: Entity, b: Entity): Vec {
+	var aPrevPos: Vec = a.getComponent.CTransform().prevPos;
+   // var y1: Vec = a.getComponent.CTransform().prevPos.y;
+    var bPrevPos: Vec = b.getComponent.CTransform().prevPos;
+   // var y2: Vec = b.getComponent.CTransform().prevPos.y;
+	//Vec2 delta(MATH.abs(x1-x2), MATH.abs(y1-y2));
+	var negDelta: Vec = aPrevPos.subtract(bPrevPos);
+	var delta: Vec = negDelta.abs();
+    //var w1: Vec = a.getComponent.CBoundingBox().halfSize.x;
+	//var h1: Vec = a.getComponent.CBoundingBox().halfSize.y;
+	var w1h1: Vec = a.getComponent.CBoundingBox().halfSize;
+    //var w2: Vec = b.getComponent.CBoundingBox().halfSize.x;
+	//var h2: Vec = b.getComponent.CBoundingBox().halfSize.y;
+	var w2h2: Vec = b.getComponent.CBoundingBox().halfSize;
+    //var ox: Vec = w1 + w2 - delta.x;
+	//var oy: Vec = h1 + h2 - delta.y;
+	var oxy: Vec = w1h1.add(w2h2).subtract(delta);
+	return oxy; //Vec(ox, oy);
 }
 
 function lineIntersect(a: Vec, b: Vec, c: Vec, d: Vec): boolean {
 	if (b == c || b == d) { return false; }
 
-	var r: Vec = b - a;
-	var s: Vec = d - c;
-	var rxs: float = r * s;
-	var cma: Vec = c - a;
-	var t: float = (cma * s) / rxs;
-	var u: float = (cma * r) / rxs;
-	return (t >= 0 && t <= 1 && u >= 0 && u <= 1);
+	var r: Vec = b.subtract(a);
+    var s: Vec = d.subtract(c);
+    var rxs: number = r.cross(s);//(r * s); 
+    var cma: Vec = c.subtract(a);
+    var t: number = cma.cross(s) / rxs;
+    var u: number = cma.cross(r) / rxs;
+
+    if (t >= 0.0 && t <= 1.0 && u >= 0.0 && u <= 1.0)
+        return true; //Vec(a.x + t*r.x, a.y + t*r.y) };
+    else
+		return false; // , Vec(0.0, 0.0) };
 
 }
 
-function entityIntersect(a: Vec, b: Vec, e: Array<Entity>): boolean {
+function entityIntersect(a: Vec, b: Vec, e: Entity): boolean {
 
-	var p: Vec = e.getComponent(CTransform).pos;
-	var bb: Vec = e.getComponent(CBoundingBox).halfSize;
+	var halfSize: Vec = e.getComponent.CBoundingBox().halfSize;
+	
+    var pos: Vec = e.getComponent.CTransform().pos;
 
+	//var c: Vec = Vec(pos.x - halfSize.x, pos.y - halfSize.y);
+	var c: Vec = pos.subtract(halfSize);
+    var d: Vec = new Vec(pos.x + halfSize.x, c.y);
+    var i1: boolean = lineIntersect(a, b, c, d);
 
-	if (lineIntersect(a, b, Vec(p.x - bb.x, p.y + bb.y), Vec(p.x + bb.x, p.y + bb.y)))
-	{ return true; }
+    d = new Vec(c.x, pos.y + halfSize.y);
+    var i2: boolean = lineIntersect(a, b, c, d);
 
-	else if (lineIntersect(a, b, Vec(p.x + bb.x, p.y + bb.y), Vec(p.x + bb.x, p.y - bb.y)))
-	{ return true; }
+    c = new Vec(pos.x + halfSize.x, pos.y - halfSize.y);
+    d = new Vec(c.x, pos.y + halfSize.y);
+    var i3: boolean = lineIntersect(a, b, c, d);
 
-	else if (lineIntersect(a, b, Vec(p.x + bb.x, p.y - bb.y), Vec(p.x - bb.x, p.y - bb.y)))
-	{ return true; }
+    c = new Vec(pos.x - halfSize.x, pos.y + halfSize.y);
+    d = new Vec(pos.x + halfSize.x, c.y);
+    var i4: boolean = lineIntersect(a, b, c, d);
 
-	else if (lineIntersect(a, b, Vec(p.x - bb.x, p.y - bb.y), Vec(p.x - bb.x, p.y + bb.y)))
-	{ return true; }
+    return i1 || i2 || i3|| i4;
 
-	else { return false; }
 
 }
 
-function lightEntityIntersect(a: Vec, b: Vec, e: Array<Entity>): boolean {
-	var p: Vec = e.getComponent(CTransform).pos;
-	p.y = 768 - p.y;
 
-	var bb: Vec = e.getComponent(CBoundingBox).halfSize;
-
-
-	if (lineIntersect(a, b, Vec(p.x - bb.x, p.y + bb.y), Vec(p.x + bb.x, p.y + bb.y)))
-	{ return true; }
-
-	else if (lineIntersect(a, b, Vec(p.x + bb.x, p.y + bb.y), Vec(p.x + bb.x, p.y - bb.y)))
-	{ return true; }
-
-	else if (lineIntersect(a, b, Vec(p.x + bb.x, p.y - bb.y), Vec(p.x - bb.x, p.y - bb.y)))
-	{ return true; }
-
-	else if (lineIntersect(a, b, Vec(p.x - bb.x, p.y - bb.y), Vec(p.x - bb.x, p.y + bb.y)))
-	{ return true; }
-
-	else { return false; }
-
-}
 
 module.exports.getOverlap = getOverlap;  
 module.exports.getPreviousOverlap = getPreviousOverlap;  
 module.exports.lineIntersect = lineIntersect;  
 module.exports.entityIntersect = entityIntersect;  
-module.exports.lightEntityIntersect = lightEntityIntersect;  
 
 
 
