f653d05be0b0d3f6f94f9bfeb4dc4d13be42219b
Eeshan Garg <jerryguitarist@gmail.com>
2019-03-20 22:42:30 -0230

    rendering: Map Entity tags to their respective animation names.
    We'll need this in the level editor when we insert an entity
    and want to cycle through all Animations that the entity with the
    given tag can have.

--- rendering/Rendering.js
+++ rendering/Rendering.js
@@ -10,10 +10,20 @@ const path = require("path");
 
 // The map which holds all the Animations loaded in from the config. 
 const animationMap = new Map();
+const tagToAnimationNames = new Map();
 const idMap =  [];
 
 /* istanbul ignore next */
 function loadAnimations(fileName: string) {
+    tagToAnimationNames.set("decoration", []);
+    tagToAnimationNames.set("effect", []);
+    tagToAnimationNames.set("player", []);
+    tagToAnimationNames.set("npc", []);
+    tagToAnimationNames.set("item", []);
+    tagToAnimationNames.set("tile", []);
+    tagToAnimationNames.set("background", []);
+    tagToAnimationNames.set("misc", []);
+
     fs.readFile(path.resolve(fileName), function(error: Object, file: Object) {
         // Read in the Animations file as a JSON.
         let content = JSON.parse(file);
@@ -21,27 +31,46 @@ function loadAnimations(fileName: string) {
 
         for (let i = 0; i < content.length; i++) {
             if (content[i].type === "static"){
-                setAnimation(content[i].AnimationName, content[i].SpriteName, content[i].SpriteName, j, j, content[i].frameCount, content[i].fps, content[i].x, content[i].y);
+                setAnimation(content[i].AnimationName,
+                             content[i].SpriteName,
+                             content[i].SpriteName,
+                             j, j, content[i].frameCount,
+                             content[i].fps, content[i].x,
+                             content[i].y);
                 idMap.push({name: content[i].SpriteName, id: j});
                 j += 1;
             }
             else {
-                setAnimation(content[i].AnimationName, content[i].SpriteNameL, content[i].SpriteNameR, j, j+1, content[i].frameCount, content[i].fps, content[i].x, content[i].y);
+                setAnimation(content[i].AnimationName,
+                             content[i].SpriteNameL,
+                             content[i].SpriteNameR,
+                             j, j+1, content[i].frameCount,
+                             content[i].fps, content[i].x,
+                             content[i].y);
                 idMap.push({name: content[i].SpriteNameR, id: j});
                 idMap.push({name: content[i].SpriteNameL, id: j+1});
                 j += 2;
             }
-            
+
+            if (content[i].tag === "npc" || content[i].tag === "player") {
+                if (!content[i].AnimationName.endsWith('Idle')) {
+                    continue;
+                }
+            }
+
+            // $FlowFixMe
+            tagToAnimationNames.get(content[i].tag).push(content[i].AnimationName);
         }
     });
-
 }
 
 
 function getAnimation(AnimationName: string): Animation {
     let x = animationMap.get(AnimationName);
     if (typeof x !== 'undefined') {
-        let copy = new Animation(x.name, x.spriteR, x.spriteL, x.rid, x.lid,x.frameCount + 1, 60 / x.fps, x.width, x.height);
+        let copy = new Animation(x.name, x.spriteR, x.spriteL,
+                                 x.rid, x.lid, x.frameCount + 1,
+                                 60 / x.fps, x.width, x.height);
         return copy;
     } else {
         let copy = new Animation('error', 'error', 'error', 0, 0, 1, 1, 1, 1);
@@ -54,15 +83,30 @@ function getAnimationIDMap(): Object {
     return idMap;
 }
 
-function setAnimation(AnimationName: string, spriteNameR: string, spriteNameL: string, rid: number, lid: number, frameCount: number, fps: number, x: number, y: number) {
-    const anim = new Animation(AnimationName, spriteNameR, spriteNameL, rid, lid, frameCount, fps, x, y);
+function setAnimation(AnimationName: string,
+                      spriteNameR: string,
+                      spriteNameL: string,
+                      rid: number, lid: number,
+                      frameCount: number, fps: number,
+                      x: number, y: number) {
+    const anim = new Animation(AnimationName, spriteNameR,
+                               spriteNameL, rid, lid,
+                               frameCount, fps, x, y);
     animationMap.set(AnimationName, anim);
 }
 
+/* istanbul ignore next */
+function getAnimationsByTag(tag: string): ?Array<string> {
+    if (tagToAnimationNames.get(tag) === undefined) {
+        return [];
+    }
+    return tagToAnimationNames.get(tag);
+}
 
 module.exports = {
     'setAnimation': setAnimation,
     'getAnimation': getAnimation,
     'loadAnimations': loadAnimations,
-    'getAnimationIDMap': getAnimationIDMap
+    'getAnimationIDMap': getAnimationIDMap,
+    'getAnimationsByTag': getAnimationsByTag
 }
